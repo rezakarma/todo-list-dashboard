02b27e7756de1d0b285da0ea50c374293bd98828
rezakarma <78763186+rezakarma@users.noreply.github.com>
2024-11-08 07:19:13 +0100

    handle situation when no todo avalibles

--- src/components/todos/todoList.tsx
+++ src/components/todos/todoList.tsx
@@ -1,16 +1,23 @@
 "use client";
+import { useTranslations } from "next-intl";
 import { Card, CardContent } from "../ui/card";
 import { ScrollArea } from "../ui/scroll-area";
 import TodoItem from "./todoItem";
 import { TodoListType as TodoListProps } from "@/types/store.types";
+import SettingItemSheet from "../settings/settingItemSheet";
+import TodoForm from "./todoForm";
+import { Button } from "../ui/button";
+import { FilePlus2 } from "lucide-react";
 
 const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+  const t = useTranslations("todos");
   return (
     <Card className="w-[80%]">
       <CardContent className="pt-6 p-2">
         <ScrollArea className="h-[500px] w-full">
           <div className="flex flex-col gap-2 px-3">
             {todos &&
+              todos.length > 0 &&
               todos.map((item) => {
                 return (
                   <TodoItem
@@ -21,6 +28,25 @@ const TodoList: React.FC<TodoListProps> = ({ todos }) => {
                   />
                 );
               })}
+            {!todos ||
+              (todos.length === 0 && (
+                <div className="w-full h-full flex-col gap-2 mt-[20%] flex justify-center items-center">
+                  <h1 className="text-2xl">
+                    {t("notAvalibleTodo")}
+                  </h1>
+                  <SettingItemSheet
+                    ItemForm={TodoForm}
+                    title={t("add")}
+                    description={t("enterTodo")}
+                    value={null}
+                  >
+                    <Button type="submit">
+                      {t("add")}
+                      <FilePlus2 />
+                    </Button>
+                  </SettingItemSheet>
+                </div>
+              ))}
           </div>
         </ScrollArea>
       </CardContent>
